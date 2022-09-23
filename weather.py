c340b831da52f89b4d82646aef6da667e9643873
Giorgio Giuliani <giorgiogiuliani@C02FC10NML85.homenet.telecomitalia.it>
2022-09-23 17:02:17 +0200

    added file creation

--- weather.py
+++ weather.py
@@ -1,5 +1,6 @@
-#City, Time, Temperature, Condition
+#Format required City, Time, Temperature, Condition
 
+from asyncore import write
 import requests
 
 openweathermap_key =''
@@ -10,17 +11,17 @@ def get_weather(city_name, api_key='a5dc08224e805ea90ec982239685a5f8' ):
     url = f'https://api.openweathermap.org/data/2.5/forecast?q={city_name}&appid={api_key}'
     r = requests.get(url)
     content = r.json()
-    s = content['list']
-    
-    for i in range(0, len(s)):
-        x = s[i]
-        orario = x['dt_txt']
-        condition = x['weather'][0]['main']
-        temperature = int(x['main']['temp']-273.15)
-        print(f'City_name: {city_name}, Time: {orario}, Condition: {condition}, Temperature: {temperature}')
-        ++i
-        #temperature
-
+    content = content['list'] #get only the content of the file, what is passed in the list of 40 dictionaries
+    with open(f'{city_name}.txt','w') as w: 
+        for i in range(0, len( content)):
+            x = content[i] #extract each dictionary for each time slot and store it in a variable
+            orario = x['dt_txt'] #get time
+            condition = x['weather'][0]['main'] 
+            temperature = int(x['main']['temp']-273.15)
+            w.write(f'City_name: {city_name}, Time: {orario}, Condition: {condition}, Temperature: {temperature}')
+            print(f'City_name: {city_name}, Time: {orario}, Condition: {condition}, Temperature: {temperature}')
+            ++i
+        
 
 get_weather(city_name='Venezia')
 
